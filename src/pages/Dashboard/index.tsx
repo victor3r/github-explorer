ca566a15e2de30b5ccc25f56bc1c02f5bca5964f
Victor Rabelo <victorabelo1697@gmail.com>
2020-07-10 23:20:24 -0300

    :sparkles: styling dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,114 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
-import { Title } from './styles';
+import logoImg from '../../assets/logo.svg';
+
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
-  return <Title>Explore repositórios no Github.</Title>;
+  return (
+    <>
+      <img src={logoImg} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+      <Form>
+        <input placeholder="Digite o nome do usuário/repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="www">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49796681?s=460&u=579efd4aa76676bd78d9bb2599fa824d7e77dd1c&v=4"
+            alt="Victor Rabelo"
+          />
+          <div>
+            <strong>victor3r/challenge-typeorm-upload</strong>
+            <p>
+              Challenge about Node.js and TypeScript including TypeORM and
+              Multer applied in GoStack Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={32} color="#CBCBD6" />
+        </a>
+        <a href="www">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49796681?s=460&u=579efd4aa76676bd78d9bb2599fa824d7e77dd1c&v=4"
+            alt="Victor Rabelo"
+          />
+          <div>
+            <strong>victor3r/challenge-typeorm-upload</strong>
+            <p>
+              Challenge about Node.js and TypeScript including TypeORM and
+              Multer applied in GoStack Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={32} color="#CBCBD6" />
+        </a>
+        <a href="www">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49796681?s=460&u=579efd4aa76676bd78d9bb2599fa824d7e77dd1c&v=4"
+            alt="Victor Rabelo"
+          />
+          <div>
+            <strong>victor3r/challenge-typeorm-upload</strong>
+            <p>
+              Challenge about Node.js and TypeScript including TypeORM and
+              Multer applied in GoStack Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={32} color="#CBCBD6" />
+        </a>
+        <a href="www">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49796681?s=460&u=579efd4aa76676bd78d9bb2599fa824d7e77dd1c&v=4"
+            alt="Victor Rabelo"
+          />
+          <div>
+            <strong>victor3r/challenge-typeorm-upload</strong>
+            <p>
+              Challenge about Node.js and TypeScript including TypeORM and
+              Multer applied in GoStack Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={32} color="#CBCBD6" />
+        </a>
+        <a href="www">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49796681?s=460&u=579efd4aa76676bd78d9bb2599fa824d7e77dd1c&v=4"
+            alt="Victor Rabelo"
+          />
+          <div>
+            <strong>victor3r/challenge-typeorm-upload</strong>
+            <p>
+              Challenge about Node.js and TypeScript including TypeORM and
+              Multer applied in GoStack Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={32} color="#CBCBD6" />
+        </a>
+        <a href="www">
+          <img
+            src="https://avatars2.githubusercontent.com/u/49796681?s=460&u=579efd4aa76676bd78d9bb2599fa824d7e77dd1c&v=4"
+            alt="Victor Rabelo"
+          />
+          <div>
+            <strong>victor3r/challenge-typeorm-upload</strong>
+            <p>
+              Challenge about Node.js and TypeScript including TypeORM and
+              Multer applied in GoStack Bootcamp
+            </p>
+          </div>
+
+          <FiChevronRight size={32} color="#CBCBD6" />
+        </a>
+      </Repositories>
+    </>
+  );
 };
 
 export default Dashboard;
